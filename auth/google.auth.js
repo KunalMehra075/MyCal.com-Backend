f47266f262dc45ca400ad3b522103328e93d754f
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-04-23 18:53:56 +0530

    DeploymentUrlsChanged

--- auth/google.auth.js
+++ auth/google.auth.js
@@ -12,7 +12,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:4500/google/callback",
+  callbackURL: "https://my-cal-com-backend.vercel.app/google/callback",
   passReqToCallback: true
 }, (request, accessToken, refrestToken, profile, done) => {
   // console.log(profile);//! -------> Consoling profile in server
